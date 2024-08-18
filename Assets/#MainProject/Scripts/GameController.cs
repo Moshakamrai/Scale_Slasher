022e0018b3f9cc31682fdeeb9c735b21ecf65f72
Nafis Fuad <47419374+Moshakamrai@users.noreply.github.com>
2024-08-19 00:17:35 +0600

    Initial commit

---
+++ Assets/#MainProject/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.InputSystem;
+public class GameController : MonoBehaviour
+{
+    //  void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         // Post the "OnPlayerHealthChange" event with an integer parameter
+    //         NotificationsManager.Instance.PostNotification("OnPlayerHealthChange", 75);
+    //     }
+    // }
+}
---
+++ Assets/#MainProject/Scripts/ListenerClass.cs
@@ -0,0 +1,28 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class ListenerClass : MonoBehaviour
+// {
+//     void Start()
+//     {
+//         // Register this object to listen for the "OnPlayerHealthChange" event
+//         NotificationsManager.Instance.AddListener("OnPlayerHealthChange", OnPlayerHealthChange);
+//     }
+
+//     void OnDestroy()
+//     {
+//         // Unregister this listener to prevent memory leaks
+//         NotificationsManager.Instance.RemoveListener("OnPlayerHealthChange", OnPlayerHealthChange);
+//     }
+
+//     // Event handler that will be called when the notification is posted
+//     void OnPlayerHealthChange(object[] parameters)
+//     {
+//         // Handle the event
+//         if (parameters.Length > 0 && parameters[0] is int health)
+//         {
+//             Debug.Log("Player health has changed to: " + health);
+//         }
+//     }
+// }
